84548ef2dbba118ace0b1ee5ffefbef22f163391
Tina Giska <tina.tylicka@gmail.com>
2018-03-21 20:31:15 +0100

    Update scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var withButtonClass = document.getElementsByClassName('button');
+
+
+for (var i = 0;  i <= withButtonClass.length-1  ; i++) {  
+   var x = withButtonClass[i];
+   var text = x.innerHTML;
+   alert(text);
+}
+
+
+
